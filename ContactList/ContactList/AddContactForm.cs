f4d97d6fc5d1b08ec815b766341db095aecb8eb7
HuyJLL <daonhathuy789@gmail.com>
2025-05-27 08:50:54 +0700

    Add form thêm liên hệ

--- ContactList/ContactList/AddContactForm.cs
+++ ContactList/ContactList/AddContactForm.cs
@@ -16,5 +16,15 @@ namespace ContactList
         {
             InitializeComponent();
         }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
